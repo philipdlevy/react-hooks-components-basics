b3f6b2a6843727a4ecae97017ba21ac199e5d75b
Philip Levy <philipdlevy@gmail.com>
2022-04-07 13:27:07 -0600

    commit tree 8c24e9b89fbaaab496e3e00a2aa7139509b7c500

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+function Article() {
+    return (
+      <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    );
+  }
+
 ReactDOM.render(<App />, document.getElementById("root"));
